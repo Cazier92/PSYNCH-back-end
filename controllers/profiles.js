c106d7f8b3d0ade6892fed01754820e045b76dc0
Cazier92 <bryce.cazier@gmail.com>
2023-02-02 19:32:49 -0800

    initial commit

---
+++ controllers/profiles.js
@@ -0,0 +1,32 @@
+import { Profile } from '../models/profile.js'
+import { v2 as cloudinary } from 'cloudinary'
+
+function index(req, res) {
+  Profile.find({})
+  .then(profiles => res.json(profiles))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+}
+
+function addPhoto(req, res) {
+  const imageFile = req.files.photo.path
+  Profile.findById(req.params.id)
+  .then(profile => {
+    cloudinary.uploader.upload(imageFile, {tags: `${req.user.email}`})
+    .then(image => {
+      profile.photo = image.url
+      profile.save()
+      .then(profile => {
+        res.status(201).json(profile.photo)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+}
+
+export { index, addPhoto }
---
+++ routes/profiles.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import * as profilesCtrl from '../controllers/profiles.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, profilesCtrl.index)
+router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+
+export { router }
