f9320fa2cc6b72b1370030d089364f55e48ec6f9
Cazier92 <bryce.cazier@gmail.com>
2023-02-04 10:29:44 -0800

    add emotionPosts routes, stub up emotionPosts controller, add public field to emotionPost model

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import './config/database.js'
 // import routes
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as emotionPostsRouter } from './routes/emotionPosts.js'
 
 // create the express app
 const app = express()
@@ -24,6 +25,7 @@ app.use(formData.parse())
 // mount imported routes
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/emotionPosts', emotionPostsRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
