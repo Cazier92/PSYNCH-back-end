d70af41fc34f0f8cac9dd4a3d5fe5942835f2bfe
Cazier92 <bryce.cazier@gmail.com>
2023-02-04 12:34:22 -0800

    update profile model to reflect change in friend request functionality (not using a friend code)

--- models/profile.js
+++ models/profile.js
@@ -1,5 +1,4 @@
 import mongoose, { Types } from 'mongoose'
-import { v4 as uuidv4 } from 'uuid'
 
 const Schema = mongoose.Schema
 
@@ -7,14 +6,8 @@ const profileSchema = new Schema({
   name: String,
   avatar: String,
   emotionPosts: [{type: Schema.Types.ObjectId, ref: 'EmotionPost'}],
-  friendRequests: [{type: String}],
+  friendRequests: [{type: Types.ObjectId, ref: 'Profile'}],
   friends: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
-  friendCode: {
-    type: String,
-    default: function() {
-      return uuidv4()
-    }
-  },
   messages: [{type: Schema.Types.ObjectId, ref: 'DirectMessage'}]
 },{
   timestamps: true,
