72d0c327e1308f9fb57b50839c6be94d2ed1c805
Yong Park <pyongho0306@gmail.com>
2023-02-07 09:26:09 -0800

    Add photo into emotionPost model

--- models/emotionPost.js
+++ models/emotionPost.js
@@ -1,54 +1,76 @@
-import mongoose, { Types } from 'mongoose'
+import mongoose, { Types } from "mongoose";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  content: {type: String, required: true, maxLength: 65},
-  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
-})
+  content: { type: String, required: true, maxLength: 65 },
+  author: { type: Schema.Types.ObjectId, ref: "Profile" },
+});
 
-const reactionSchema = new Schema({
-  reaction: {type: String, 
-    required: true,
-    enum: [
-      'Like', 'Celebrate', 'Support', 'Funny', 'Love', 'Curious'
-    ]
+const reactionSchema = new Schema(
+  {
+    reaction: {
+      type: String,
+      required: true,
+      enum: ["Like", "Celebrate", "Support", "Funny", "Love", "Curious"],
+    },
+    author: { type: Schema.Types.ObjectId, ref: "Profile" },
   },
-  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
-}, {
-  timestamps: true,
-})
+  {
+    timestamps: true,
+  }
+);
 
-const emotionPostSchema = new Schema({
-  emotion: {type: String, 
-    required: true,
-    enum: [
-      // Bad - Let's call this 'Down' to avoid stigmatizing (open to other suggestions):
-      'Bored', 'Stressed', 'Tired',
-      // Fearful:
-      'Anxious', 'Rejected', 'Scared',
-      // Angry:
-      'Mad', 'Jealous', 'Betrayed',
-      // Disgusted:
-      'Embarrassed', 'Disgusted',
-      // Sad:
-      'Lonely', 'Guilty', 'Hurt',
-      // Happy:
-      'Optimistic', 'Peaceful', 'Powerful', 'Accepted', 'Joyful',
-      // Surprised:
-      'Startled', 'Confused', 'Excited', 'Amazed'
-
-    ]
+const emotionPostSchema = new Schema(
+  {
+    emotion: {
+      type: String,
+      required: true,
+      enum: [
+        // Bad - Let's call this 'Down' to avoid stigmatizing (open to other suggestions):
+        "Bored",
+        "Stressed",
+        "Tired",
+        // Fearful:
+        "Anxious",
+        "Rejected",
+        "Scared",
+        // Angry:
+        "Mad",
+        "Jealous",
+        "Betrayed",
+        // Disgusted:
+        "Embarrassed",
+        "Disgusted",
+        // Sad:
+        "Lonely",
+        "Guilty",
+        "Hurt",
+        // Happy:
+        "Optimistic",
+        "Peaceful",
+        "Powerful",
+        "Accepted",
+        "Joyful",
+        // Surprised:
+        "Startled",
+        "Confused",
+        "Excited",
+        "Amazed",
+      ],
+    },
+    content: { type: String, maxLength: 65 },
+    photo: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: "Profile" },
+    comments: [commentSchema],
+    reactions: [reactionSchema],
+    public: { type: Boolean, required: true, default: true },
   },
-  content: {type: String, maxLength: 65},
-  author: {type: Schema.Types.ObjectId, ref: 'Profile'},
-  comments: [commentSchema],
-  reactions: [reactionSchema],
-  public: {type: Boolean, required: true, default: true}
-},{
-  timestamps: true,
-})
+  {
+    timestamps: true,
+  }
+);
 
-const EmotionPost = mongoose.model('EmotionPost', emotionPostSchema)
+const EmotionPost = mongoose.model("EmotionPost", emotionPostSchema);
 
-export { EmotionPost }
+export { EmotionPost };
