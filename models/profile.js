673e1a9e1bb501ac1a89a994d95e1faa64f1c7f5
k-vernon <kvernon908@gmail.com>
2023-02-08 10:37:09 -0500

    change user default

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: {type: String, default: "http://res.cloudinary.com/dmbhhnc2j/image/upload/v1675833662/ryynzj1woe6izknd9cf9.jpg"},
+  photo: {type: String, default: "https://res.cloudinary.com/dmbhhnc2j/image/upload/v1675870453/AdobeStock_349497933_xq1qwl.jpg"},
   emotionPosts: [{type: Schema.Types.ObjectId, ref: 'EmotionPost'}],
   friendRequests: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
   friends: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
