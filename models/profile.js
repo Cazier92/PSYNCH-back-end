1a6cb2ca3352470fbd61e8ef05d160948dada830
Cazier92 <bryce.cazier@gmail.com>
2023-02-07 21:22:29 -0800

    update default pic

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: {type: String, default: "http://res.cloudinary.com/dmbhhnc2j/image/upload/v1675832170/hpjrbk5pw14swjkkqqjn.svg"},
+  photo: {type: String, default: "http://res.cloudinary.com/dmbhhnc2j/image/upload/v1675833662/ryynzj1woe6izknd9cf9.jpg"},
   emotionPosts: [{type: Schema.Types.ObjectId, ref: 'EmotionPost'}],
   friendRequests: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
   friends: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
